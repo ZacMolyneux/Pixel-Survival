b50d76d9b799d70def9aea7aca035a9fe83a2178
zac <zac.molyneux@hotmail.com>
2016-12-14 18:38:53 +1100

    added directional player and follow camera.

---
+++ Assets/_Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject Player;
+
+    public float offset = 4;
+
+    void Update()
+    {
+        //transform.position
+        transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + offset, -20);
+    }
+
+}
---
+++ Assets/_Scripts/SpriteController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        CheckMousePos();
+	}
+
+    void CheckMousePos()
+    {
+        if (Input.mousePosition.x < Screen.width / 2)//mouse is on left side
+        {
+            Debug.Log("LEFT");
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+        else
+        {
+            Debug.Log("Right");
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+
+    }
+}
